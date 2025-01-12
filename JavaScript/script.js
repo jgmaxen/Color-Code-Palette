bc92b39489ed46a6d0a31a80602dd57bd5f47e89
Devilwife <nicole.barger22@gmail.com>
2025-01-12 17:04:52 -0500

    pull

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,15 +13,22 @@ const savedColor5El = document.querySelector(".color-5");
 const generateColorBtn = document.getElementById("generate-color");
 const savePaletteBtn = document.getElementById("save-palette");
 
-const clearLastBtn = document.getElementById("clear-last");
-const clearAllBtn = document.getElementById("clear-all");
+const emptyLastBtn = document.getElementById("empty-last");
+const emptyAllBtn = document.getElementById("empty-all");
 
 generateColorBtn.addEventListener("click", () => {
     const redValue = redColorInput.value;
     const greenValue = greenColorInput.value;
     const blueValue = blueColorInput.value;
-
-    const rgbValue1 = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
-
+    const rgbValue1 = `rgb(${redValue}, ${greenValue}, ${blueValue})`;   
     savedColor1El.style.backgroundColor = rgbValue1;
-})
+    savedColor1El.innerHTML = rgbValue1;
+});
+generateColorBtn.addEventListener("click", () => {
+    const redValue = redColorInput.value;
+    const greenValue = greenColorInput.value;
+    const blueValue = blueColorInput.value;
+    const rgbValue2 = `rgb(${redValue}, ${greenValue}, ${blueValue})`; 
+    savedColor2El.style.backgroundColor = rgbValue2;    
+    savedColor2El.innerHTML = rgbValue2;
+}); 
